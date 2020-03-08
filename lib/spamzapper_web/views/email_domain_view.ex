656f3f706c02384ece077d557066211572c8c847
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-03-08 14:20:33 +0000

    Replace ban email with a calculated status
    Quite simply checks whether the `ban_email` field is set to something
    then either spits out a red "Blacklisted" label, or a plain "OK" one.

--- lib/spamzapper_web/views/email_domain_view.ex
+++ lib/spamzapper_web/views/email_domain_view.ex
@@ -1,3 +1,20 @@
 defmodule SpamzapperWeb.EmailDomainView do
   use SpamzapperWeb, :view
+
+  def banned(ban), do: !!ban.ban_email
+
+  def ban_class(ban) do
+    case ban_status(ban) do
+      "Blacklisted" -> "danger"
+      "OK" -> "light"
+    end
+  end
+
+  def ban_status(ban) do
+    if banned(ban) do
+      "Blacklisted"
+    else
+      "OK"
+    end
+  end
 end
