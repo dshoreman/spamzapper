23915efb5ba373a09d07925b6e60c7a2a1f2c101
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-03-09 05:51:56 +0000

    Add basic whitelist indications
    Purely visual for the moment, but this adds a short list of somewhat
    trusted email providers which can later be used to prevent new global
    bans being applied to them. Unless of course a matching ban already
    exists for that particular email domain, in which case the blacklist
    will take priority.
    
    Just in case, we also add `true` as a fallback if somehow none of the
    other options in our condition end up matching to any of the handlers.

--- lib/spamzapper_web/views/email_domain_view.ex
+++ lib/spamzapper_web/views/email_domain_view.ex
@@ -1,20 +1,36 @@
 defmodule SpamzapperWeb.EmailDomainView do
   use SpamzapperWeb, :view
 
+  @whitelist [
+    "aol.com",
+    "btinternet.com",
+    "gmail.com",
+    "gmx.com",
+    "hotmail.com",
+    "hotmail.co.uk",
+    "live.com",
+    "outlook.com",
+    "yahoo.com",
+    "yahoo.co.uk",
+  ]
+
   def banned(ban), do: !!ban.ban_email
 
   def ban_class(ban) do
     case ban_status(ban) do
       "Blacklisted" -> "danger"
-      "OK" -> "light"
+      "Whitelisted" -> "success"
+      "Unknown" -> "warning"
+      "Neutral" -> "light"
     end
   end
 
   def ban_status(ban) do
-    if banned(ban) do
-      "Blacklisted"
-    else
-      "OK"
+    cond do
+      banned(ban) -> "Blacklisted"
+      ban.email_domain in @whitelist -> "Whitelisted"
+      !ban.ban_email -> "Neutral"
+      true -> "Unknown"
     end
   end
 end
